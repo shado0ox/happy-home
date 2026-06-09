4cf302b3b39cc41e574506894fcfb06c85f1f1d1
shado0ox <shady.nasif@gmail.com>
2026-06-09 21:26:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — البيت السعيد للمصاريف v2
-const CACHE_NAME = 'albait-alsaeid-v2.0.4';
+const CACHE_NAME = 'albait-alsaeid-v2.0.5';
 const STATIC_ASSETS = [
   './',
   './index.html',
